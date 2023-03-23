4b6707f2b00c41840699d4ab0aaeda0e4d3a6e2e
sponnet <stefaan@ponnet.com>
2023-03-23 11:54:42 +0100

    update & download certificates from dappmanager

--- build/files/dlcerts.sh
+++ build/files/dlcerts.sh
@@ -1,15 +1,30 @@
-#!/bin/bash
-while :
-do
-    echo "Downloading certificates"
-    wget -O /etc/nginx/certs/my.ava.do.crt.temp http://iso.ava.do/my.ava.do.crt
-    wget -O /etc/nginx/certs/my.ava.do.key.temp  http://iso.ava.do/my.ava.do.key    
-    supervisorctl stop main
-    supervisorctl stop nginx
-    mv -f /etc/nginx/certs/my.ava.do.crt.temp /etc/nginx/certs/my.ava.do.crt
-    mv -f /etc/nginx/certs/my.ava.do.key.temp /etc/nginx/certs/my.ava.do.key
-    supervisorctl start nginx
+#!/bin/sh
+while true; do
+    mkdir -p /tmp
+    date > /tmp/reload-certs.txt
+
+    echo "Updating SSL certificates"
+
+    md5sumbefore=$(md5sum "/etc/nginx/certs/my.ava.do.crt")
+    curl "http://dappmanager.my.ava.do/my.ava.do.crt" --output /etc/nginx/certs/my.ava.do.crt --silent
+    curl "http://dappmanager.my.ava.do/my.ava.do.key" --output /etc/nginx/certs/my.ava.do.key --silent
+    md5sumafter=$(md5sum "/etc/nginx/certs/my.ava.do.crt")
+
+    echo "Downloaded newest SSL certificates"
+
+    if [ "$md5sumbefore" != "$md5sumafter" ]; then
+        echo "Restarting processes after SSL update"        
+        supervisorctl restart main
+        supervisorctl restart nginx
+    else
+        echo "No SSL updates"        
+    fi
+    # Start main program if not running already.
     supervisorctl start main
-    # 5 days
-    sleep 432000
+    supervisorctl start nginx
+
+    echo "SSL updater going to sleep for 24h"
+
+    #sleep one day
+    sleep 86400
 done
